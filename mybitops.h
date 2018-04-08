1f61f23517e6d54e460629a4d7a7c4b8ea5838bf
omid <asudeh.1@osu.edu>
2018-04-07 20:09:26 -0400

    parallel does not get performance

--- mybitops.h
+++ mybitops.h
@@ -1,5 +1,6 @@
 #ifndef MYBITOPS_H
 #define MYBITOPS_H
+#include <iostream>
 #include <vector>
 #include <boost/dynamic_bitset.hpp> 
 using namespace std;
@@ -8,19 +9,11 @@ class mybitops{
 	mybitops(){}
 	~mybitops(){}
 	////###################### Bit Operation ###################################
-	/*
+/*
 	 *Perform logic AND operation directly based on compressed bitvectors 
 	 */
-	vector<size_t> logic_and(vector<size_t> vector1, vector<size_t> vector2);
-	/*
-	 *Perform logic AND operation directly based on compressed bitvectors 
-	 */
-	vector<size_t> logic_and_ref(vector<size_t> &vector1, vector<size_t> &vector2);
-
-	/*
-	 * Perform logic OR operation directly based on compressed bitvectors
-	 */ 
-	vector<size_t> logic_or(vector<size_t> vector1, vector<size_t> vector2);
+	 vector<size_t> logic_and_ref(vector<size_t>* v1,vector<size_t>* v2, size_t v1begin, size_t  v1end,
+                                        size_t v2begin, size_t  v2end );
 	/*
 	 * Use WAH compression algorithm to compress one bitvector
 	 * note: randomness increase the compression time
@@ -30,20 +23,6 @@ class mybitops{
 	 * Use WAH decompression algorithm to decompress all the bitvector
 	 */ 
 	boost::dynamic_bitset<> uncompressIndex(vector<size_t> cvector, size_t uncompressed_size);
-	/*
-	 * Counts the number of setbits upto a certain position
-	 */
-	//  int CountOnesUpto(vector<size_t> &compresed_vector, int position);
-	 /*
-	 * Returns the word type:
-	 * 0: literal 
-	 * 1: zeros
-	 * 2: ones
-	 */
-	 /*
-	 * Part of the WAH algorithm
-	 * Check if the current word is a fill word or a literal word
-	 */ 
 	 int ismyfill(size_t word1);
 	 /*
 	  * return the word type
@@ -52,11 +31,6 @@ class mybitops{
 	  * 2:
 	  */
 	 int word_type(size_t word1);
-	//  void print_compressed(vector<size_t>);
-	/*
-	 * just for the purpose of testing the CountOnesUpto function
-	 */
-	// void test_count();
 	vector<size_t> parallel_and(vector<size_t> &vector1, vector<size_t> &vector2);	
 };
 #endif
