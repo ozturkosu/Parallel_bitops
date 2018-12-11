cb88df403e8ede9fc371b07df72afa67f5d815e8
ozturkosu <ozturk.osu@gmail.com>
2018-12-11 16:12:30 -0500

    comment

--- openmp_version/main.cu
+++ openmp_version/main.cu
@@ -414,7 +414,7 @@ __global__ void parallel_and_kernel_V2(vector<size_t> &vector1, vector<size_t> &
 
 */
 
-
+/*
 
 __global__ void parallelAndDevice(size_t * Vector1, int Vector1_size, size_t * prefixSum1, size_t * wordSize1  , size_t * Vector2,
 	int Vector2_size, size_t * prefixSum2 , size_t * wordSize2 ,size_t * outVector, size_t vector1_bit_length, size_t vector2_bit_length)
@@ -427,6 +427,7 @@ __global__ void parallelAndDevice(size_t * Vector1, int Vector1_size, size_t * p
 
 }
 
+*/
 
 int main(int argc, char** argv)
 {
