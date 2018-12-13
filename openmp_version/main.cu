68645e7d9f6e4aa572fba617d110d87f33fcbca1
ozturkosu <ozturk.osu@gmail.com>
2018-12-12 23:51:48 -0500

    a

--- openmp_version/main.cu
+++ openmp_version/main.cu
@@ -445,7 +445,7 @@ int main(int argc, char** argv)
     // cout<<"parallel_and time:"<< t3-t2<<endl;
 
     clock_t t4 = clock();
-    Bitops.logic_and_ref(&comp_bitset1,&comp_bitset2,0,comp_bitset1.size()-1,0,comp_bitset2.size()-1);
+    //Bitops.logic_and_ref(&comp_bitset1,&comp_bitset2,0,comp_bitset1.size()-1,0,comp_bitset2.size()-1);
     clock_t t5 = clock();
     cout<<"seq_and time:"<< t5-t4<<endl;
     //pthread_exit(NULL);
