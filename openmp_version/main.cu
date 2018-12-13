a6888d2449c37f10838ac33ebc247d73c2c6c003
ozturkosu <ozturk.osu@gmail.com>
2018-12-12 22:18:17 -0500

    a

--- openmp_version/main.cu
+++ openmp_version/main.cu
@@ -412,7 +412,7 @@ int main(int argc, char** argv)
     if(DATA_GENERATION)
     {
 				cout<<"data generation*\n";
-        size_t items = 100000000;
+        size_t items = 120000000;
         float fill_percent = .001;
         boost::dynamic_bitset<> tempvector(items);
         cout<<"Data Generation"<<endl;
