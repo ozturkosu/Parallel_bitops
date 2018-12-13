16634a9ca623790456f05f43dc20082130b28ee1
ozturkosu <ozturk.osu@gmail.com>
2018-12-13 00:14:11 -0500

    deneme

--- openmp_version/main.cu
+++ openmp_version/main.cu
@@ -66,7 +66,7 @@ __device__ size_t myBinarySearchGPU(size_t* arr, size_t l, size_t r, size_t x)
 
 }
 
-/*
+
 
 __global__ void parallelAndDevice2(size_t * Vector1, int Vector1_size,  size_t * Vector2,
 	int Vector2_size ,size_t * outVector, size_t vector1_bit_length, size_t vector2_bit_length)
@@ -75,8 +75,8 @@ __global__ void parallelAndDevice2(size_t * Vector1, int Vector1_size,  size_t *
 
 		 size_t min_bit_length = (vector1_bit_length<vector2_bit_length)?vector1_bit_length:vector2_bit_length;
 
-		 size_t* word_lengths1 = new size_t[vec1_size]; //vector of length of the words
- 		 size_t* prefix_sum1 = new size_t[vec1_size]; // prefix-sum generated form word_lengths1
+		 size_t* word_lengths1 = new size_t[Vector1_size]; //vector of length of the words
+ 		 size_t* prefix_sum1 = new size_t[Vector1_size]; // prefix-sum generated form word_lengths1
  		 size_t pre_sum1 = 0;
  		 size_t vector1_bit_length = 0;
 
@@ -96,8 +96,8 @@ __global__ void parallelAndDevice2(size_t * Vector1, int Vector1_size,  size_t *
 
 
 
-		 size_t* word_lengths2 = new size_t[vec2_size]; //vector of length of the words
-		 size_t* prefix_sum2 = new size_t[vec2_size];   // prefix-sum generated form word_lengths1
+		 size_t* word_lengths2 = new size_t[Vector2_size]; //vector of length of the words
+		 size_t* prefix_sum2 = new size_t[Vector2_size];   // prefix-sum generated form word_lengths1
 		 size_t pre_sum2 = 0;
 		 size_t vector2_bit_length = 0;
 
@@ -116,11 +116,273 @@ __global__ void parallelAndDevice2(size_t * Vector1, int Vector1_size,  size_t *
 		 }
 
 
+		 size_t *v1_ptr = &Vector1[0];
+		size_t *v2_ptr = &Vector2[0];
+
+
+		size_t b1 = tid*CHUNK_SIZE;
+		size_t b2 = (tid+1)*CHUNK_SIZE;
+
+		////2.2 calc the word area
+		int w11 = myBinarySearchGPU(prefixSum1,0,Vector1_size-1,b1);
+		int w12 = myBinarySearchGPU(prefixSum1,0,Vector1_size-1,b2);
+
+		int w21 = myBinarySearchGPU(prefixSum2,0,Vector2_size-1,b1);
+		int w22 = myBinarySearchGPU(prefixSum2,0,Vector2_size-1,b2);
+
+		float res_size = ((w12-w11)<(w22-w21))?(w12-w11):(w22-w21);
+
+		size_t min_length = res_size * 1.01;//give 1% more capacity
+		//size_t *result_vector = new size_t[min_length] ;
+		size_t *result_vector = &outVector[0] ;
+		size_t *result_ptr = &result_vector[0] ;
+
+		size_t ones = 0, zeros = 0;
+		size_t it1 = w11;
+		size_t it2 = w21;
+		size_t word1 = Vector1[it1];
+		size_t word2 = Vector2[it2];
+		size_t count = 0;
+
+
+		while(it1 <= w12 && it2 <= w22) {
+			if(ismyfill_device(word1) && ismyfill_device(word2)) {  //both are fill words
+				size_t c1 = word1 & 0x3fffffff;         //get the len of fill word
+				size_t c2 = word2 & 0x3fffffff;         //get the len of fill word
+				size_t isone1 = word1 & 0x40000000;     //check if it is one
+				size_t isone2 = word2 & 0x40000000;     //check if it is one
+
+				if(c1 == c2) {
+					if(isone1 && isone2) {
+						//allZeros = false;
+						ones += c1;
+						if(zeros > 0) {
+							size_t n = 0x80000000 + zeros;
+							zeros = 0;
+							result_ptr[count] = n;
+							count++;
+							// result_vector.push_back(n);
+						}
+					} else {
+						zeros += c1;
+						if(ones > 0) {
+							size_t n = 0xc0000000 + ones;
+							ones = 0;
+							result_ptr[count] = n;
+							count++;
+							// result_vector.push_back(n);
+						}
+					}
+					word1 = Vector1[++it1];
+					word2 = Vector2[++it2];
+				} else if (c1 > c2) {
+					if(isone1 && isone2) {
+						//allZeros = false;
+						ones += c2;
+						if(zeros > 0) {
+							size_t n = 0x80000000 + zeros;
+							zeros = 0;
+							result_ptr[count] = n;
+							count++;
+							// result_vector.push_back(n);
+						}
+					} else {
+						zeros += c2;
+						if(ones > 0) {
+							size_t n = 0xc0000000 + ones;
+							ones = 0;
+							// result_vector.push_back(n);
+						}
+					}
+					if(isone1)
+						word1 = 0xc0000000 + (c1 - c2);
+					else
+						word1 = 0x80000000 + (c1 - c2);
+					word2 = Vector2[++it2];
+				} else { //c2 > c1
+					if(isone1 && isone2) {
+						//allZeros = false;
+						ones += c1;
+						if(zeros > 0) {
+							size_t n = 0x80000000 + zeros;
+							zeros = 0;
+							result_ptr[count] = n;
+							count++;
+							// result_vector.push_back(n);
+						}
+					} else {
+						zeros += c1;
+						if(ones > 0) {
+							size_t n = 0xc0000000 + ones;
+							ones = 0;
+							result_ptr[count] = n;
+							count++;
+							// result_vector.push_back(n);
+						}
+					}
+					if(isone2)
+						word2 = 0xc0000000 + (c2 - c1);
+					else
+						word2 = 0x80000000 + (c2 - c1);
+					word1 = Vector1[++it1];
+				}
+			} else if(ismyfill_device(word1) && !ismyfill_device(word2)) { //one fill, one literal
+				size_t c1 = word1 & 0x3fffffff; //lenth
+				size_t isone1 = word1 & 0x40000000;  //is one?
+
+				if(isone1) { //if one, depend on literal
+					if(ones > 0) {
+						size_t n = 0xc0000000 + ones;
+						//allZeros = false;
+						ones = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					if(zeros > 0) {
+						size_t n = 0x80000000 + zeros;
+						zeros = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					result_ptr[count] = word2;
+					count++;
+					// result_vector.push_back(word2);
+				} else { //id zero, generate a zero word
+					if(ones > 0) {
+						size_t n = 0xc0000000 + ones;
+						ones = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					zeros += 31;
+				}
+				if(c1 == 31){
+					word1 = Vector1[++it1];
+				}
+				else {
+					if(isone1)
+						word1 = 0xc0000000 + (c1 - (size_t)31);
+					else
+						word1 = 0x80000000 + (c1 - (size_t)31);
+				}
+				word2 = Vector2[++it2];
+			} else if(!ismyfill_device(word1) && ismyfill_device(word2)) { //one literal, one fill
+				size_t c2 = word2 & 0x3fffffff; //lenth
+				size_t isone2 = word2 & 0x40000000;  //is one?
+
+				if(isone2) { //if one, depend on literal
+					if(ones > 0) {
+						size_t n = 0xc0000000 + ones;
+						//allZeros = false;
+						ones = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					if(zeros > 0) {
+						size_t n = 0x80000000 + zeros;
+						zeros = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					//if (word1 != 0) //allZeros = false;
+					result_ptr[count] = word1;
+					count++;
+					// result_vector.push_back(word1);
+				} else { //id zero, generate a zero word
+					if(ones > 0) {
+						size_t n = 0xc0000000 + ones;
+						ones = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					zeros += 31;
+				}
+				if(c2 == 31)
+					word2 = Vector2[++it2];
+				else {
+					if(isone2)
+						word2 = 0xc0000000 + (c2 - (size_t)31);
+					else
+						word2 = 0x80000000 + (c2 - (size_t)31);
+				}
+				word1 = Vector1[++it1];
+			} else { //both are literal words
+				size_t num = word1 & word2;
+				if(num == 0) {
+					zeros += 31;
+					if(ones > 0) {
+				//allZeros = false;
+						size_t n = 0xc0000000 + ones;
+						ones = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+				} else if (num == 0x7fffffff) {
+					ones += 31;
+					//allZeros = false;
+					if(zeros > 0) {
+						size_t n = 0x80000000 + zeros;
+						zeros = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+				} else {
+					if(ones > 0) {
+						//allZeros = false;
+						size_t n = 0xc0000000 + ones;
+						ones = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n);
+					}
+					if(zeros > 0) {
+						size_t n = 0x80000000 + zeros;
+						zeros = 0;
+						result_ptr[count] = n;
+						count++;
+						// result_vector.push_back(n); //// freq access
+					}
+					//if (num != 0) //allZeros = false;
+					result_ptr[count] = num;
+					count++;
+					// result_vector.push_back(num);//// freq access
+				}
+				word1 = Vector1[++it1];
+				word2 = Vector2[++it2];
+			}
+		}
+		if(ones > 0) {
+		//allZeros = false;
+			size_t n = 0xc0000000 + ones;
+			ones = 0;
+			result_ptr[count] = n;
+			count++;
+			// result_vector.push_back(n);
+		}
+		if(zeros > 0) {
+			size_t n = 0x80000000 + zeros;
+			zeros = 0;
+			result_ptr[count] = n;
+			count++;
+			// result_vector.push_back(n);
+		}
+
+
+
+
 
 
 }
 
-*/
+
 
 __global__ void parallelAndDevice(size_t * Vector1, int Vector1_size, size_t * prefixSum1, size_t * wordSize1  , size_t * Vector2,
 	int Vector2_size, size_t * prefixSum2 , size_t * wordSize2 ,size_t * outVector, size_t vector1_bit_length, size_t vector2_bit_length)
@@ -412,7 +674,7 @@ int main(int argc, char** argv)
     if(DATA_GENERATION)
     {
 				cout<<"data generation*\n";
-        size_t items = 40000000;
+        size_t items = 160000000;
         float fill_percent = .001;
         boost::dynamic_bitset<> tempvector(items);
         cout<<"Data Generation"<<endl;
